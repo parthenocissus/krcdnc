14018cedfef696b0050a47a4437ab42a870fde3e
parthenocissus <!smokve21>
2022-01-03 18:19:28 +0100

    edits

--- static/space/afroatlas/afroatlas.js
+++ static/space/afroatlas/afroatlas.js
@@ -79,7 +79,7 @@ $(document).ready(function () {
         "                    gladi</em>.«</p>\n" +
         "            <p>Stomak gladi. Ulica <a href=\"#\">Nezavisnosti</a>. Tugaljiv kurati ponos.</p>";
 
-    $(".mark").on('click tap touchstart', () => {
+    $(".mark").on('click tap touchstart touchend', () => {
         console.log("mark clicked...");
         let note = $(".note"),
             noteTxtCont = $(".note-text");
@@ -90,7 +90,7 @@ $(document).ready(function () {
         note.css({"visibility": "visible"});
     });
 
-    $("#note-x").on('click tap touchstart', () => {
+    $("#note-x").on('click tap touchstart touchend', () => {
         $(".note").css({"visibility": "hidden"});
     });
 
