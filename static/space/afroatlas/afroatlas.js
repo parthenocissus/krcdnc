67576dda4b52e6b5dfcdefc131056168bfdbd912
parthenocissus <!smokve21>
2022-01-04 13:11:31 +0100

    edits

--- static/space/afroatlas/afroatlas.js
+++ static/space/afroatlas/afroatlas.js
@@ -86,13 +86,22 @@ $(document).ready(function () {
 
                 // Handle pinch
                 this.hammer.on('pinchstart pinchmove', function (ev) {
+
+                    const el = ev.target;
+                    const rect = el.getBoundingClientRect();
+                    const pos = {
+                      x: (ev.center.x - rect.left),
+                      y: (ev.center.y - rect.top)
+                    };
+
                     // On pinch start remember initial zoom
                     if (ev.type === 'pinchstart') {
-                        initialScale = instance.getZoom()
-                        instance.zoomAtPoint(initialScale * ev.scale, {x: ev.center.x, y: ev.center.y})
+                        initialScale = instance.getZoom();
+                        // instance.zoomAtPoint(initialScale * ev.scale, {x: ev.center.x, y: ev.center.y});
+                        instance.zoomAtPoint(initialScale * ev.scale, {x: pos.x, y: pos.y});
                     }
 
-                    instance.zoomAtPoint(initialScale * ev.scale, {x: ev.center.x, y: ev.center.y})
+                    instance.zoomAtPoint(initialScale * ev.scale, {x: pos.x, y: pos.y})
                 });
 
                 // Prevent moving the page on some devices when panning over SVG
