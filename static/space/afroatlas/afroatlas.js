c617fd8cf9bff5430e0e5c0e1d6de26b3e3ff8b6
parthenocissus <!smokve21>
2022-01-04 13:27:29 +0100

    edits

--- static/space/afroatlas/afroatlas.js
+++ static/space/afroatlas/afroatlas.js
@@ -87,22 +87,22 @@ $(document).ready(function () {
                 // Handle pinch
                 this.hammer.on('pinchstart pinchmove', function (ev) {
 
-                    // const el = ev.target;
-                    // const rect = el.getBoundingClientRect();
-                    // const pos = {
-                    //   x: (ev.center.x - rect.left),
-                    //   y: (ev.center.y - rect.top)
-                    // };
+                    const el = ev.target;
+                    const rect = el.getBoundingClientRect();
+                    const pos = {
+                      x: (ev.center.x - rect.left),
+                      y: (ev.center.y - rect.top)
+                    };
 
                     // On pinch start remember initial zoom
                     if (ev.type === 'pinchstart') {
                         initialScale = instance.getZoom();
                         // instance.zoomAtPoint(initialScale * ev.scale, {x: pos.x, y: pos.y});
-                        instance.zoomAtPoint(initialScale * ev.scale, {x: ev.center.x - 200, y: ev.center.y});
+                        instance.zoomAtPoint(initialScale * ev.scale, {x: pos.x, y: ev.center.y});
                     }
 
                     // instance.zoomAtPoint(initialScale * ev.scale, {x: pos.x, y: pos.y})
-                    instance.zoomAtPoint(initialScale * ev.scale, {x: ev.center.x - 200, y: ev.center.y});
+                    instance.zoomAtPoint(initialScale * ev.scale, {x: pos.x, y: ev.center.y});
                 });
 
                 // Prevent moving the page on some devices when panning over SVG
