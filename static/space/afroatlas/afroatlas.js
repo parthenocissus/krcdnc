29d0bd98c67d3a6132fbafa2b501b7a172246418
parthenocissus <!smokve21>
2022-01-06 18:56:20 +0100

    edits

--- static/space/afroatlas/afroatlas.js
+++ static/space/afroatlas/afroatlas.js
@@ -144,24 +144,4 @@ $(document).ready(function () {
 
     /* Notes Functionality */
 
-    // let noteTxt = "<p>Lazar i Marko putuju do vulkana. »Pogled mi klizne kroz prozor, u baruštine i sirotinjska sela. Krave se\n" +
-    //     "                vuku kroz blato kao tužni, olindrali kosturi. Pogrbljena baka tegli na leđima divovski naramak drva.\n" +
-    //     "                Mršava deca naduvenih trbuha, u poderanim gaćama, stoje kraj puta i mašu. Kvašiorkor, <em>stomak\n" +
-    //     "                    gladi</em>.«</p>\n" +
-    //     "            <p>Stomak gladi. Ulica <a href=\"#\">Nezavisnosti</a>. Tugaljiv kurati ponos.</p>";
-
-    // $(".mark").on('click tap touchstart touchend', () => {
-    //     let note = $(".note"),
-    //         noteTxtCont = $(".note-text");
-    //     note.css({"visibility": "hidden"});
-    //     noteTxtCont.html(noteTxt);
-    //     let noteH = noteTxtCont.height();
-    //     note.height(noteH + 16);
-    //     note.css({"visibility": "visible"});
-    // });
-
-    $("#note-x").on('click tap touchstart touchend', () => {
-        $(".note").css({"visibility": "hidden"});
-    });
-
 });
