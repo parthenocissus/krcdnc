4e25a11df6847521f40874ae39d9de3040af52fb
parthenocissus <!smokve21>
2022-03-18 13:21:28 +0100

    edits

--- static/space/svesvrstani/mojazastava.js
+++ static/space/svesvrstani/mojazastava.js
@@ -29,15 +29,73 @@ $(document).ready(function () {
     });
 
     $("#gen-secundum").click(() => {
-       generate();
+        generate();
     });
 
     let generate = () => {
         console.log("generating...");
+        $("#flag").show();
+        window.scrollTo(0, $("#flag").offset().top - 30);
     };
 
     $(".continue").click(() => {
-       $("#continued").show();
+        $("#continued").show();
+    });
+
+    let svg2pngDownloader = () => {
+        let s = {w: 600, h: 400};
+
+        let flagSvg = $("#flag-svg");
+        flagSvg.attr({
+            "width": "150px",
+            "height": "100px"
+        });
+
+        let svg = $("#flag").html();
+
+        flagSvg.removeAttr("width");
+        flagSvg.removeAttr("height");
+
+        let canvas = document.createElement("canvas");
+        canvas.width = s.w;
+        canvas.height = s.h;
+        let context = canvas.getContext('2d');
+
+        let v = canvg.Canvg.fromString(context, svg, {
+            ignoreDimensions: true,
+            scaleWidth: s.w,
+            scaleHeight: s.h,
+            offsetX: 56.25,
+            offsetY: 37.5
+        });
+        v.start();
+
+        let img = canvas.toDataURL("image/png");
+        const link = document.createElement('a');
+        link.download = 'flag.png';
+        link.href = canvas.toDataURL("image/png");
+        link.click();
+    }
+
+    $(".get-flag").click(() => {
+
+        let failed = $(".failed");
+        failed.hide();
+
+        let valid = true;
+        $(".required").each(function () {
+            let element = $(this);
+            if (element.val() === "") {
+                valid = false;
+            }
+        });
+
+        if (valid) {
+            svg2pngDownloader();
+        } else {
+            failed.show();
+        }
+
     });
 
 });
