a711e8d9308d858a0d627bb0807350becf3fecac
parthenocissus <!smokve21>
2022-10-20 17:28:42 +0200

    edits

--- static/space/svesvrstani/svesvrstani.js
+++ static/space/svesvrstani/svesvrstani.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 
     let mainMenuWidth = "18%";
     let mainMenuTop = "3vw";
+    let mainMenuTopMobile = "0";
+    let mobileQuery = "(max-width: 650px)";
 
     let changeLine = (id, x1, y1, x2, y2) => {
         d3.select(id).transition()
@@ -15,31 +17,57 @@ $(document).ready(function () {
             .attr("y2", y2);
     }
 
-    let menuOn = () => {
+    let changeLineOn = () => {
         changeLine("#hamburgerLine1", 1.3, 18.8, 18.8, 1.3);
         changeLine("#hamburgerLine2", 1.3, 1.3, 18.8, 18.8);
+    }
+
+    let changeLineOff = () => {
+        changeLine("#hamburgerLine1", 0, 6.2, 20, 6.2);
+        changeLine("#hamburgerLine2", 0, 13.8, 20, 13.8);
+    }
+
+    let menuOn = () => {
+        changeLineOn();
         $("#gray-fog").fadeIn();
         $("#main-menu").animate({
-            // width: mainMenuWidth
             top: mainMenuTop
         }, 400, () => {
             $("#vertical-menu").fadeIn(180);
         });
-        // $("#main-menu").width("300px");
+        toggle = false;
+    }
+
+    let menuOnMobile = () => {
+        changeLineOn();
+        $("#main-menu").fadeIn(300, () => {
+            $("#vertical-menu").fadeIn(180);
+        });
         toggle = false;
     }
 
     let menuOff = () => {
-        changeLine("#hamburgerLine1", 0, 6.2, 20, 6.2);
-        changeLine("#hamburgerLine2", 0, 13.8, 20, 13.8);
+        changeLineOff();
         $("#vertical-menu").fadeOut(180, () => {
             $("#gray-fog").fadeOut();
             $("#main-menu").animate({
-                // width:'0'
                 top: "100vh"
             });
         });
         toggle = true;
+    }
+
+    let menuOffMobile = () => {
+        changeLineOff();
+        $("#vertical-menu").fadeOut(180, () => {
+            $("#main-menu").fadeOut(180);
+        });
+        toggle = true;
+    }
+
+    if (window.matchMedia(mobileQuery).matches) {
+        menuOn = menuOnMobile;
+        menuOff = menuOffMobile;
     }
 
     d3.select(".menu").on("click", () => {
